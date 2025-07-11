a532e284ded7022a55c41217c64044dc7492e100
Hao Nguyen <nguyentansonghao2019@gmail.com>
2025-07-11 15:08:26 +0700

    Setup celery and offload crawling operation to background queue

--- background_tasks/crawler_tasks.py
+++ background_tasks/crawler_tasks.py
@@ -1,30 +1,10 @@
-from playwright.async_api import async_playwright, Page
+from playwright.async_api import async_playwright
 from urllib.parse import urljoin
 import asyncio
-from .models import CrawlUrlsRequestBody, CreateWaitingUrl
+from crawler.models import CrawlUrlsRequestBody, CreateWaitingUrl
 from utils.url_handler import get_domain, get_domain_url
-from .repository import insert_url
-
-async def get_all_hrefs(page: Page):
-    hrefs = await page.eval_on_selector_all(
-        "a[href]",
-        """(elements) => elements.map(el => el.href.trim()).filter(href => href.length > 0)"""
-    )
-    return hrefs
-
-async def scroll_all_page(page: Page, wait_for: int = 1000):
-    prev_height = await page.evaluate("() => document.documentElement.scrollHeight")
-    print(f"Inital height: {prev_height}")
-    while True:
-        await page.evaluate("() => window.scrollBy(0, document.documentElement.scrollHeight)")
-        await page.wait_for_timeout(wait_for)
-        curr_height = await page.evaluate("() => document.documentElement.scrollHeight")
-        if curr_height == prev_height:
-            print("Stopped (no more content).")
-            break
-        prev_height = curr_height
-        print(f"Current Height: {curr_height}")
-
+from crawler.repository import insert_url
+from utils.crawl_handler import get_all_hrefs, scroll_all_page
 
 async def fetch_urls(request_body: CrawlUrlsRequestBody):
     urls = set()
